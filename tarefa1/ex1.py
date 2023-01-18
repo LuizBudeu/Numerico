3b7e1b9915bbde091eda92283132c0f9ebdb84ca
Luiz Budeu <lugui@usp.br>
2023-01-17 21:00:56 -0300

    adicionando radianos

--- tarefa1/ex1.py
+++ tarefa1/ex1.py
@@ -9,7 +9,7 @@ y2 = np.cos(x) * 2
 y3 = np.cos(x) * 3
 
 plt.plot(x, y1, 'g-', x, y2, 'g--', x, y3, 'g-.')
-plt.xlabel('x, entre [-pi, pi]') 
+plt.xlabel('x (radianos), entre [-pi, pi]') 
 plt.ylabel('y(x)')
 plt.title('Tarefa 1 - Gráfico de diferentes funções cossenos')
 plt.legend(['cos(x)', 'cos(2x)', 'cos(3x)'])      
