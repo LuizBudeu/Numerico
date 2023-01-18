62a42d8bea6c06cbe36a120db022fa02831b8581
vinicius-CL <vinicius_lopes15@usp.br>
2023-01-17 21:18:59 -0300

    Pequena correção

--- tarefa1/ex1.py
+++ tarefa1/ex1.py
@@ -4,9 +4,9 @@ import numpy as np
 
 x = np.arange(-np.pi, np.pi, 0.1)
 
-y1 = np.cos(x) * 1
-y2 = np.cos(x) * 2
-y3 = np.cos(x) * 3
+y1 = np.cos(x * 1)
+y2 = np.cos(x * 2)
+y3 = np.cos(x * 3)
 
 plt.plot(x, y1, 'g-', x, y2, 'g--', x, y3, 'g-.')
 plt.xlabel('x (radianos), entre [-pi, pi]') 
