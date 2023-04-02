ac96d0f3b0f568b52ad518a5364dcf6b9057e409
Luiz Budeu <lugui@usp.br>
2023-04-02 19:51:06 -0300

    sir euler e trapezio

--- atividade2/sir_euler.py
+++ atividade2/sir_euler.py
@@ -1,54 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 from constants import *
-
-
-# # Condições iniciais
-# S0 = 10E6 - 1
-# I0 = 1
-# R0 = 0
-
-# # Equações diferenciais
-# def SIR_model(t, y, beta, gamma):
-#     S, I, R = y
-#     dSdt = -beta*S*I
-#     dIdt = beta*S*I - gamma*I
-#     dRdt = gamma*I
-#     return np.array([dSdt, dIdt, dRdt])
-
-# # Parâmetros
-# beta = 0.4
-# gamma = 0.1
-
-# # Intervalo de tempo
-# t = np.linspace(0, 100, 1)
-
-# # Número de pontos
-# n = len(t)
-
-# # Função que calcula os valores dos pontos usando o método dos trapézios
-# def trapezoidal(f, a, b, n, y0):
-#     h = (b - a) / float(n)
-#     w = np.zeros((n, len(y0)))
-#     w[0] = y0
-#     for i in range(1, n):
-#         w[i] = w[i-1] + h/2 * (f(t[i-1], w[i-1], beta, gamma) + f(t[i], w[i-1] + h*f(t[i-1], w[i-1], beta, gamma), beta, gamma))
-#     return  w
-
-# # Implementando o método dos trapézios para resolver o modelo SIR
-# y = trapezoidal(SIR_model, 0, 100, n, np.array([S0, I0, R0]))
-
-# print(y)
-
-# # Plotando os resultados
-# plt.plot(range(0, 100), y[:,0], label='Suscetíveis')
-# plt.plot(range(0, 100), y[:,1], label='Infectados')
-# plt.plot(range(0, 100), y[:,2], label='Recuperados')
-# plt.xlabel('Tempo')
-# plt.ylabel('Número de Indivíduos')
-# plt.title('Modelo SIR com o Método dos Trapézios')
-# plt.legend()
-# plt.show()
 
 
 def SIR_model(Y, t):
@@ -59,28 +12,22 @@ def SIR_model(Y, t):
     return np.array([dotS, dotI, dotR])
 
 
-# Método dos Trapézios
-def trapezios(Y0, t, h):
+def Euler(Y0, t, h):
     nt = len(t)
     Y  = np.zeros([nt, len(Y0)])
     Y[0] = Y0
-    for i in range(1, len(t)):
-        Y[i] = succesive_aprox(Y[i-1], t[i], h)
+    for i in range(nt-1):
+        Y[i+1] = Y[i] + SIR_model(Y[i], t[i]) * h
     return Y
 
 
-def succesive_aprox(y_ant, t, h, max_iter = 100, precisao = 1E-12):
-   
-    y_guess = y_ant
-
-    for _ in range(max_iter):
-        # Phi de Newton
-        y_next_guess = y_ant + h*(SIR_model(y_guess, t) + SIR_model(y_ant, t-h))/2
-        if abs(np.linalg.norm(y_next_guess) - np.linalg.norm(y_guess)) < precisao:
-            break
-        y_guess = y_next_guess
-
-    return y_next_guess
+# # Exact solution
+# def exact(t):
+#     return np.array([
+#         -0.3*np.e**t + 0.5*np.e**(2*t) + 0.8*np.e**(3*t),
+#         0.3*np.e**t + 0.2*np.e**(3*t),
+#         0.2*np.e**(3*t)
+#     ])
 
 
 def tabela(n):
@@ -91,7 +38,7 @@ def tabela(n):
         # Time points
         t = np.linspace(t0, T, num=n[i]+1) # Para incluir T
 
-        y = trapezios(Y0, t, h[i])
+        y = Euler(Y0, t, h[i])
         y_T.append(y)
         
         # if i > 0:
@@ -133,5 +80,3 @@ def gerar_grafico(t_n, y_n):
 
 
 a = tabela(n)
-
-
